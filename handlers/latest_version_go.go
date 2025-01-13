c564d9411fb93e8eab4c9e56ac306183b3fce8c1
Brent Laster <bclaster@nclasters.org>
2025-01-13 11:55:24 -0500

    Update latest_version_go.go

--- handlers/latest_version_go.go
+++ handlers/latest_version_go.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"encoding/json"
+	"os"
 )
 
 func LatestVersionGo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Latest Go Version Called")
-	req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, "https://go.dev/dl/?mode=json", nil)
+	req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, "https://go.dev/dl/?mode=json&include=all", nil)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -30,4 +32,16 @@ func LatestVersionGo(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Something unrecoverable went wrong")
 		return
 	}
+
+	// Parse the JSON response
+	var versions []GoVersion
+	if err := json.NewDecoder(resp.Body).Decode(&versions); err != nil {
+	   return "", err
+	}
+	
+	// The latest version is the first item in the array
+	if len(versions) > 0 {
+	   return versions[0].Version, nil
+	}
+	return "", fmt.Errorf("no versions found")
 }
